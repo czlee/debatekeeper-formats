7cbcec8881936d2e4d0974c6c0d5bcc68fecb0db
Chuan-Zheng Lee <czlee@alumni.stanford.edu>
2021-09-28 12:02:18 +1300

    Add version numbers

--- update_list.py
+++ update_list.py
@@ -4,7 +4,7 @@ committed to the repository."""
 
 import argparse
 import json
-import xml.etree.ElementTree as etree
+from lxml import etree
 from pathlib import Path
 
 parser = argparse.ArgumentParser(description=__doc__)
@@ -16,6 +16,8 @@ if not args.formats_dir.is_dir():
     print(f"{formats_dir} is not a directory")
     exit(1)
 
+validator = etree.RelaxNG(etree.parse("schema-2.1.rng"))
+
 formats = []
 
 for child in args.formats_dir.iterdir():
@@ -24,12 +26,17 @@ for child in args.formats_dir.iterdir():
         continue
 
     child_root = etree.parse(open(child))
-    info = child_root.find("info")
 
+    if not validator.validate(child_root):
+        err = validator.error_log.last_error
+        print(f"Validation error in {child.name}, line {err.line}: {err.message}")
+
+    info = child_root.find("info")
     formats.append({
         "filename": child.name,
         "name": child_root.find("name").text,
         "url": f"https://formats.debatekeeper.czlee.nz/formats/{child.name}",
+        "version": int(child_root.find("version").text),
         "region": [e.text for e in info.findall("region")],
         "level": [e.text for e in info.findall("level")],
         "used-at": [e.text for e in info.findall("used-at")],
